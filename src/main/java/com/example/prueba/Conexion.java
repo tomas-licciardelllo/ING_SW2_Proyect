5d82049e2b9bfd7c775e05cd690bd9e23d671629
PepeObot <tobotoro@gmail.com>
2025-08-31 22:21:47 -0300

    hice algo de cliente y en conexión no sé que carajos se cambdio pq no toqué nada

--- src/main/java/com/example/prueba/Conexion.java
+++ src/main/java/com/example/prueba/Conexion.java
@@ -4,7 +4,7 @@ import java.sql.*;
 public class Conexion{
 
     private static final String URL = "...";
-    private static final String USER = "...";   //Según la profe de ciberseguridad no hay que poner root
+    private static final String USER = "...";   //Según la profe de ciberseguridad no hay que poner root || QUE LA MAMEISHON, a parte no vamos a tener que iniciar sesión
     private static final String PASSWORD = "...";
 
     public static  Connection getConnection() throws SQLException{
--- src/main/java/com/example/prueba/cliente.java
+++ src/main/java/com/example/prueba/cliente.java
@@ -1,4 +1,37 @@
 package com.example.prueba;
 
 public class cliente {
+    private String nombre;
+    private String Telefono;
+    private String Seguro;
+
+    public cliente(String nombre, String telefono, String seguro) {
+        this.nombre = nombre;
+        Telefono = telefono;
+        Seguro = seguro;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        Telefono = telefono;
+    }
+
+    public String getSeguro() {
+        return Seguro;
+    }
+
+    public void setSeguro(String seguro) {
+        Seguro = seguro;
+    }
 }
