da67f3f1d620183983038fcd2a98c82bdf2c6d54
PepeObot <tobotoro@gmail.com>
2025-09-01 16:53:54 -0300

    SIGUE PAI

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,10 @@ module com.example.prueba {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
-    requires com.google.gson;
+    //requires com.google.gson;
     requires java.net.http;
     requires java.sql;
+    requires com.google.gson;
 
 
     opens com.example.prueba to javafx.fxml;
